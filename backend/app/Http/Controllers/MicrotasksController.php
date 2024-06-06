796e52880f841e368a53a840fb3ab44e31fa1c07
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-06-06 14:37:11 +0200

    create migration

---
+++ backend/app/Http/Controllers/MicrotasksController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\microtasks;
+use App\Http\Requests\StoremicrotasksRequest;
+use App\Http\Requests\UpdatemicrotasksRequest;
+
+class MicrotasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoremicrotasksRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(microtasks $microtasks)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(microtasks $microtasks)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatemicrotasksRequest $request, microtasks $microtasks)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(microtasks $microtasks)
+    {
+        //
+    }
+}
